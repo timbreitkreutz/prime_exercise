c644b1fabb7753579dca94b6100c7f32d6bf701a
Tim Breitkreutz <tim@generalthings.com>
2014-04-02 11:59:32 -0700

    eliminated an extra line

--- prime_table.rb
+++ prime_table.rb
@@ -5,8 +5,7 @@ require './generator'
 size = 10
 primes = Generator::primes(size)
 # Figure out how wide to make the columns
-width = Math::log10(primes.last ** 2).to_i + 1
-width_format = " %#{width}d"
+width_format = " %#{Math::log10(primes.last ** 2).to_i + 1}d"
 
 (0..size-1).each do |ii|
   (0..size-1).each do |jj|
